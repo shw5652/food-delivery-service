d705d554062958903e26085141729907135cc2b7
Shweta Kumari <sonishweta5652@gmail.com>
2025-09-07 17:31:34 +0530

    changed the port.

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -6,11 +6,14 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (req, res) => res.json({
+    status: 'ok'
+}));
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
