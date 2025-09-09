5e69a273c69645639d2f2c73df010667f898a43e
Shweta Kumari <sonishweta5652@gmail.com>
2025-09-09 14:02:32 +0530

    seperated ports.

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => res.json({
+    status: 'ok'
+}));
 
 app.use(bodyParser.json());
 
@@ -13,5 +21,5 @@ app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 4002;
 app.listen(PORT, () =>{
-    console.log(`User service running on port ${PORT}`);
+    console.log(`User service running on http://localhost:${PORT}`);
 });
