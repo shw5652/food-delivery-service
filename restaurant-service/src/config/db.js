106894bf1b6c6c34755deb51f227a53242dbd9cd
Shweta Kumari <sonishweta5652@gmail.com>
2025-09-10 11:05:08 +0530

    Corrected db config.

--- restaurant-service/src/config/db.js
+++ restaurant-service/src/config/db.js
@@ -1,9 +1,15 @@
 import pkg from "pg";
-const { Pool } = pkg;
+const {Pool} = pkg;
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false} : false,
+    max: parseInt(process.env.DB_MAX_CLIENTS) || 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 export default pool;
